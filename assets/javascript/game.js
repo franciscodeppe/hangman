c882092fdf5cca1a14d4363bac3f24952577f55e
Francisco Deppe <fdeppe@LB.local>
2017-05-11 15:55:34 -0700

    stuck

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,25 @@
-var wordbank =["Foo Fighters","Metallica", "Ludacris", "Fallout Boy", "Nirvana", ""]
+var wordBank = ["foo fighters", "metallica", "tool", "jimi hendrix", "nirvana",]
 
-var letters = ["a", "b", "c", "d", "e", "" ]
+
+// startGame()
+
+// function startGame() {
+  var guesses = 0
+
+  var word = wordBank[Math.floor(Math.random() * wordBank.length)]
+
+  console.log(word)
+
+  var brokenDown = list(word)
+  console.log(brokenDown)
+
+  console.log(brokenDown.length)
+
+  // reset the guess and success array at each round. Array of letters (first array, for succesful guesses)
+  // reset the wrong guesses from the previous round. Array of letters (second arrays, one for fails)\
+  var numBlanks = brokenDown.length
+  for (var i = 0; i < numBlanks; i++) {
+
+    getElementById("answerBlanks")
+  }
+}
